882f43c60d4bf6dbc2bfdb6516d3ed9190963c9d
Walm12 <bo3ps4ec@hotmail.com>
2025-08-07 03:34:10 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@
 const firebaseConfig = {
   apiKey: "AIzaSyBqh1FQgDO6oxuFMF7feaBQdJxT0mzNmsQ",
   authDomain: "ubicacion-pruebas.firebaseapp.com",
-  databaseURL: "https://ubicacion-pruebas-default-rtdb.firebaseio.com/",
+  databaseURL: "https://console.firebase.google.com/u/1/project/datosdeubicacion/database/datosdeubicacion-default-rtdb/data/~2F",
   projectId: "ubicacion-pruebas",
   storageBucket: "ubicacion-pruebas.appspot.com",
   messagingSenderId: "989875849360",
